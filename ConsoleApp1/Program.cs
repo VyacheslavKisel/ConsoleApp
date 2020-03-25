99621cec54004fb3161b3051bb1784fcbb1b34e8
VyacheslavKisel <kisel.vyacheslav@gmail.com>
2020-03-25 14:10:29 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            Console.WriteLine(number);
+        }
+    }
+}
