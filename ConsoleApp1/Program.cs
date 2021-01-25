0d1c759a852daa15a175a2b0bef120699053234b
Kysil Viacheslav <viacheslav.kysil@gmail.com>
2021-01-26 01:39:31 +0200

    New

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -20,19 +20,13 @@ namespace ConsoleApp1
         {
             Console.WriteLine("zz2");
             Console.WriteLine("Task1");
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
             Console.WriteLine(58);
-=======
             Console.WriteLine(10);
             Console.WriteLine(8);
->>>>>>> Stashed changes
-=======
             Console.WriteLine(58);
             Console.WriteLine(10);
             Console.WriteLine(8);
             Console.WriteLine("ff");
->>>>>>> Stashed changes
         }
     }
 }
